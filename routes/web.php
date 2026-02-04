372ef35c75d23a75880685ba2c27b0a37ada805a
Subhadip <subhadip2510@gmail.com>
2026-02-04 22:49:23 +0530

    Comment out custom routes before pulling from main

--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,14 @@ Route::get('/', function () {
 //     return view('about');
 // });
 
-Route::get('/admin_login', function () {
+// Route::get('/admin_login', function () {
 
-    return view('admin_login');
-});
-Route::post('/submit', [AdminController::class, 'login']);
-Route::get('/dashboard', function () {
-    if (!session()->has('admin_id')) {
-        return redirect('/admin_login');
-    }
-    return view('dashboard');
-});
+//     return view('admin_login');
+// });
+// Route::post('/submit', [AdminController::class, 'login']);
+// Route::get('/dashboard', function () {
+//     if (!session()->has('admin_id')) {
+//         return redirect('/admin_login');
+//     }
+//     return view('dashboard');
+// });
